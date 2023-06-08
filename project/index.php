7d76682a1d19aef4f070149cfcd19bc390fb7589
Bence Stáb <bence.stab@fundoo.ch>
2023-06-08 21:31:25 +0200

    php template creation

---
+++ project/index.php
@@ -0,0 +1,9 @@
+<?php
+include "./components/meta/meta.php";
+include "./components/header/header/header.php";
+include "./components/nav/nav.php";
+include "./components/main/main_start.php";
+include "./components/contents/sections.php";
+include "./components/main/main_end.php";
+include "./components/footer/footer.php";
+?>
