fe9a668618e7427019650864d00b6b687920dd26
Bence Stáb <bence.stab@fundoo.ch>
2023-06-09 16:45:23 +0200

    header fixed

--- project/index.php
+++ project/index.php
@@ -1,6 +1,6 @@
 <?php
 include "./components/meta/meta.php";
-include "./components/header/header/header.php";
+include "./components/header/header.php";
 include "./components/nav/nav.php";
 include "./components/main/main_start.php";
 include "./components/contents/sections.php";
