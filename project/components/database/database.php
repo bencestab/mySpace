17b1376ecc4ea87594aec7476a165ea2f98b90a2
bobbyfromgeminus <info.geminus@gmail.com>
2023-06-12 21:24:03 +0200

    SQL connection fixed

--- project/components/database/database.php
+++ project/components/database/database.php
@@ -1,9 +1,10 @@
 <?php
 
-$host = 'localhost';
-$user = 'sqluser';
-$password = '123456';
-$database = 'myspace_db';
+//$host = 'localhost';
+$host =		'mysql';		// a localhost a konténerben nem működik, ezért hálózati aliast kell használni
+$user =		'sqluser';
+$password =	'123456';
+$database =	'myspace_db';
 
 $db = new mysqli($host, $user, $password, $database);
 if (!$db->set_charset('utf8')) {
