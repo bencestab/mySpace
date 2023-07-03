e08a0c2cec1c3165516d0b6f15094bd8bf193dc5
bobbyfromgeminus <info.geminus@gmail.com>
2023-07-03 19:59:55 +0200

    MySQL Connection with mysqli and PDO

--- project/components/database/database.php
+++ project/components/database/database.php
@@ -1,15 +1,31 @@
 <?php
 
-//$host = 'localhost';
-$host =		'mysql';		// a localhost a konténerben nem működik, ezért hálózati aliast kell használni
-$user =		'sqluser';
-$password =	'123456';
-$database =	'myspace_db';
+    $host 		= "mysql";          // a localhost a konténerben nem működik, ezért hálózati aliast kell használni
+    $user 		= "sqluser";
+    $password	= "123456";
+    $database	= "myspace_db";
+    $charset 	= 'utf8mb4';
 
-$db = new mysqli($host, $user, $password, $database);
-if (!$db->set_charset('utf8')) {
-    echo "error";
-    exit();
-}
+    // MYSQL Connection with MYSQLI
+        $db = new mysqli($host, $user, $password, $database);
+        if (!$db->set_charset('utf8')) {
+            echo "error";
+            exit();
+        }
 
+
+    // MySQL Connection with PDO
+        $connection = "mysql:host=$host;dbname=$database;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+            
+        try {
+            $pdo = new PDO($connection, $user, $password, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+	
 ?>
--- project/components/nav/nav.php
+++ project/components/nav/nav.php
@@ -2,16 +2,30 @@
         <mark></mark>
         <section>
             <?php
-            $sql = 'SELECT * FROM navigation WHERE nav_state=1 ORDER BY nav_id ASC';
+
+            $query = 'SELECT * FROM navigation WHERE nav_state=1 ORDER BY nav_id ASC';
+
+            // MYSQLI
+            $sql = $query;
             $result = $db->query($sql);
             while ($row = $result->fetch_assoc()) {
                 echo '<a href="/">'.$row['nav_title'].'</a>';
             }
+
+			// PDO
+			$stmt = $pdo->query($query);
+			while ($row = $stmt->fetch()) {
+                echo '<a href="/">'.$row['nav_title'].'</a>';
+            }
             ?>
-            <!--a href="/">Home</a>
-            <a href="">About</a>
-            <a href="">Info</a>
-            <a href="">Contact</a>
-            <button>X</button-->
+
+
+            <!-- STATIC HTML NAV TEMPLATE
+                a href="/">Home</a>
+                <a href="">About</a>
+                <a href="">Info</a>
+                <a href="">Contact</a>
+                <button>X</button>
+            -->
         </section>
 </nav>
